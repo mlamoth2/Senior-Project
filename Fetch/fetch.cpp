40501748b7b8d01da6eed395c5f30e01cf935d76
Developer_1 <cachemoneyswag@gmail.com>
2020-01-13 18:04:32 -0500

    additions to solution and updates to fetch

--- Fetch/fetch.cpp
+++ Fetch/fetch.cpp
@@ -1,11 +1,18 @@
 #include "..\Common\typedefs.hpp"
 #include "..\Common\opcodes.hpp"
 
-using namespace MSP430_Opcodes; // this has to be above the include below, otherwise compile error
-
 #include "fetch.hpp"
 
 namespace Fetcher
 {
 
+		MSP430Fetch::MSP430Fetch(uint16 programCounter)
+		{
+			this->programCounter = programCounter;
+		}
+
+		MSP430Fetch::~MSP430Fetch()
+		{
+		
+		}
 }
--- Fetch/fetch.hpp
+++ Fetch/fetch.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+namespace Fetcher
+{
+	class MSP430Fetch
+	{
+	public:
+
+		MSP430Fetch(uint16 programCounter);
+		~MSP430Fetch();
+
+		void advanceProgramCounter()
+		{
+			this->programCounter += sizeof(uint16);
+		}
+
+		void retractProgramCounter()
+		{
+			this->programCounter -= sizeof(uint16);
+		}
+
+		void setProgramCounter(uint16 newProgramCounter)
+		{
+			programCounter = newProgramCounter;
+		}
+
+		uint16 getProgramCounter()
+		{
+			return programCounter;
+		}
+
+	private:
+
+		uint16 programCounter;
+	};
+}
